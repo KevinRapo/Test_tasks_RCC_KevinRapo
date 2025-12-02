a1a77935ea916cc89c7717e71bd58b17dcf18222
KevinRapo <kevin.rapo@hotmail.com>
2025-12-02 19:42:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Task 2
+
+This project analyzes **CGMES EQ profile instance** files (RDF/XML) using Python, focusing on power system element extraction, validation, and querying.
+
+##  Task 1
+### Summary
+
+1.	Make a graph and additional metrics to support the assessment of the amount of activation of aFRR reserves in relation to the imbalance. Provide also reasoning and background for the assessment.
+
+![alt text](images/afrr.png)
+
+![alt text](images/cumulative.png)
+
+---
+
+## Task 2
+
+Some of the questions are further expanded on in the Jupyter Notebook file.
+
+### Summary
+
+1.	**What is the total production capacity of the generators in the model? What is each generating unit power factor?**
+
+  - Max production capacity: 1500 kW, each unit has a power factor of 0.9.
+
+2.	**What kind of regulation control generator NL-G1 has? Why this and do you know any other kinds?**
+
+    - Voltage regulated, this works by adjusting the reactive power provided
+    - Examples of other kind of control are: active power, reactive power and power factor.
+
+3.	**What are the nominal voltages of the windings of the transformer NL_TR2_2 (ID: _2184f365-8cd5-4b5d-8a28-9d68603bb6a4)? What type of transformer is this in the grid?**
+  - High end: 220 kV, low end - 15.75 kV.
+  - This is a two winding step-up transformer
+
+4.	**What is permanently allowed limit for line segment NL-Line_5 (ID: _e8acf6b6-99cb-45ad-b8dc-16c7866a4ddc) and temporarily allowed. What is difference between those limits. List other types of limits in the grid**
+
+  - Temporary is the limit allowed for a short duration while under critical condition. Permament is the regularly allowed line limit.
+5.	**Which generator is set as slack in the model? Why does model need slack node?**
+  - My conclusion from the available information is that the slack generator could not be determined from this model. My research yielded that either the "AngleRefTopologicalNode" or "referencePriority" is needed for this evaluation, both of which are missing from this model. Maybe there is another way to determine this from the model but I could not indentify it.
+  - The slack node is used as a reference point for the power system and to balance out active and reactive power in the system.
+
+6.	**Find mistakes in the model (both semantic, power system related and logical errors are present)**
+
+  #### Semantic
+  To get the file running first some xml definitions needed to be fixed
+  - Missmatch of some opening and closing tags
+  ![alt text](task2/images/tags.png)
+  - Line 678, 701 and 724 had the same ID for the "PowerTransformerEnd", which is not allowed, changed the ID endings so every ID was unique.
+
+  #### Power system/logic related
+  - Missing "baseVoltage" elements for all the windings
+  - Swapped PATL/TATL values
+  - Missing slack generator definition
+
---
+++ task1/task1.py
@@ -0,0 +1,128 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+
+URL_BASE = "https://api-baltic.transparency-dashboard.eu/"
+API_SINGLE = "api/v1/export"
+API_MULTIPLE = "api/v1/export-multiple" # Did not work due to how "," is formatted in the "requests" library. 
+
+import os 
+
+print(os.getcwd())
+
+# Helpder fucntion that 
+def fetch_(url, api, params):
+    r = requests.get(url+api,params)
+    print("\nFetch code:",r.status_code,"")
+    return r
+
+
+# Loop through the list of ids to request using the single export API
+ids = ["activations_afrr", "imbalance_volumes_v2"]
+responses = []
+for _id in ids:
+    PARAMS = {
+        "id": _id,
+        "start_date": "2025-09-22T00:00",
+        "end_date": "2025-09-23T00:00",
+        "output_time_zone": "EET",
+        "output_format": "json",
+        "json_header_groups": "1"
+    }
+
+    r = fetch_(URL_BASE, API_SINGLE, PARAMS)
+    obj = r.json()
+
+    with open(f"task1/{_id}.json", "w", encoding="utf-8") as f:
+        json.dump(obj, f, indent=4, ensure_ascii=False)
+
+    responses.append(obj)
+
+# Build the dataframes with the appropiate columns
+dataframes = []
+for obj in responses:
+    data = obj["data"]
+    columns = data["columns"]
+    ts = data["timeseries"]
+
+    # Build column names using grouping
+    col_names = [f"{col['group_level_0']}_{col['label']}" for col in columns]
+
+    rows = []
+    for block in ts:
+        row = {"timestamp": block["_from"]}
+
+        # zip column names with values
+        for name, value in zip(col_names, block["values"]):
+            row[name] = value
+
+        rows.append(row)
+
+    df = pd.DataFrame(rows)
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    print(df)
+    df = df.set_index("timestamp",drop=True)
+    print(df)
+    dataframes.append(df)
+
+
+# Extract data for plotting, renaming the imbalance columns from the previous result.
+afrr_data = dataframes[0]
+imbalance_data = dataframes[1]
+imbalance_data = imbalance_data.rename(columns={
+                                       "Estonia_None" : "Estonia_imbalance",
+                                       "Latvia_None" : "Latvia_imbalance",
+                                       "Lithuania_None" : "Lithuania_imbalance"
+                                        })
+
+
+# Plot the results
+fig, (ax_afrr, ax_v2) = plt.subplots(2, 1, figsize=(12, 8))
+
+# aFRR plotting
+styles_afrr = {
+    "Estonia_Upward": "b-",
+    "Estonia_Downward": "b--",
+    "Latvia_Upward": "r-",
+    "Latvia_Downward": "r--",
+    "Lithuania_Upward": "g-",
+    "Lithuania_Downward": "g--",
+}
+afrr_data.plot(
+    title="aFRR",
+    ylabel = "MW",
+    legend=False,
+    ax=ax_afrr,
+    style=styles_afrr
+)
+ax_afrr.grid(True, which = "both", axis="both")
+ax_afrr.legend(loc = "upper left",
+                bbox_to_anchor = (0.85, 1.2),
+                fontsize =  "small"
+)
+
+# imbalance plotting
+styles_imbalance = {
+    "Estonia_imbalance" : "b-",
+    "Latvia_imbalance" : "r-",
+    "Lithuania_imbalance" : "g-"
+}
+imbalance_data.plot(
+    title="imbalance",
+    ylabel = "MWh",
+    legend=False,
+    ax=ax_v2,
+    style=styles_imbalance
+)
+ax_v2.grid(True, which = "both", axis="both")
+ax_v2.legend()
+
+# Cumulative data
+print("\n")
+print("Cumulative aFRR activations during the day [MW]:\n\n",afrr_data.sum())
+print("\n")
+print("Cumulative imbalance during the day [MWh]:\n\n",imbalance_data.sum())
+
+plt.tight_layout(pad=2.0)
+plt.show()
